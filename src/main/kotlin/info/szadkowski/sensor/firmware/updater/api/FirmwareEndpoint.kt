3ab230f2f48061762283f6f23d9aeb40420ac8c3
Adam Szadkowski <adam@szadkowski.info>
2021-05-02 22:13:47 +0200

    Return status code for existing device

--- src/main/kotlin/info/szadkowski/sensor/firmware/updater/api/FirmwareEndpoint.kt
+++ src/main/kotlin/info/szadkowski/sensor/firmware/updater/api/FirmwareEndpoint.kt
@@ -1,12 +1,19 @@
 package info.szadkowski.sensor.firmware.updater.api
 
+import info.szadkowski.sensor.firmware.updater.domain.FirmwareRepository
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import javax.inject.Inject
 
 @Controller
-class FirmwareEndpoint {
+class FirmwareEndpoint(
+    @Inject private val firmwareRepository: FirmwareRepository,
+) {
 
     @Get(uri = "/firmware/{device}", produces = ["text/plain"])
-    fun firmware(device: String): HttpResponse<Any> = HttpResponse.notModified()
+    fun firmware(device: String): HttpResponse<Any> = when(firmwareRepository.getNewestVersionFor(device)) {
+        null -> HttpResponse.notModified()
+        else -> HttpResponse.ok()
+    }
 }
--- src/test/kotlin/info/szadkowski/sensor/firmware/updater/api/FirmwareEndpointTest.kt
+++ src/test/kotlin/info/szadkowski/sensor/firmware/updater/api/FirmwareEndpointTest.kt
@@ -1,5 +1,7 @@
 package info.szadkowski.sensor.firmware.updater.api
 
+import io.micronaut.context.annotation.Property
+import io.micronaut.context.annotation.PropertySource
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.client.annotation.Client
@@ -10,15 +12,26 @@ import strikt.assertions.isEqualTo
 import javax.inject.Inject
 
 @MicronautTest
+@PropertySource(
+    Property(name = "firmware.devices[0].id", value = "a-1"),
+    Property(name = "firmware.devices[0].versions[0]", value = "0.1"),
+)
 class FirmwareEndpointTest(
     @Inject private val client: FirmwareClient,
 ) {
 
     @Test
     fun `not found version is marked as not modified`() {
-        val firmware = client.firmware("a-1")
+        val firmware = client.firmware("a-0")
 
         expectThat(firmware).isEqualTo(HttpStatus.NOT_MODIFIED)
+    }
+
+    @Test
+    fun `return newer firmware`() {
+        val firmware = client.firmware("a-1")
+
+        expectThat(firmware).isEqualTo(HttpStatus.OK)
     }
 
     @Client("/")
