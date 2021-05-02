13348a1a7db0cdf753a53b09216c53028240edeb
Adam Szadkowski <adam@szadkowski.info>
2021-05-02 22:20:41 +0200

    Validate current version

--- src/main/kotlin/info/szadkowski/sensor/firmware/updater/api/FirmwareEndpoint.kt
+++ src/main/kotlin/info/szadkowski/sensor/firmware/updater/api/FirmwareEndpoint.kt
@@ -1,9 +1,11 @@
 package info.szadkowski.sensor.firmware.updater.api
 
 import info.szadkowski.sensor.firmware.updater.domain.FirmwareRepository
+import info.szadkowski.sensor.firmware.updater.domain.model.FirmwareVersion
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Header
 import javax.inject.Inject
 
 @Controller
@@ -12,8 +14,15 @@ class FirmwareEndpoint(
 ) {
 
     @Get(uri = "/firmware/{device}", produces = ["text/plain"])
-    fun firmware(device: String): HttpResponse<Any> = when(firmwareRepository.getNewestVersionFor(device)) {
-        null -> HttpResponse.notModified()
-        else -> HttpResponse.ok()
+    fun firmware(
+        device: String,
+        @Header(name = "x-ESP8266-version") version: String,
+    ): HttpResponse<Any> {
+        val newestVersionFor = firmwareRepository.getNewestVersionFor(device)
+        return when {
+            newestVersionFor == null -> HttpResponse.notModified()
+            newestVersionFor <= FirmwareVersion.of(version) -> HttpResponse.notModified()
+            else -> HttpResponse.ok()
+        }
     }
 }
--- src/test/kotlin/info/szadkowski/sensor/firmware/updater/api/FirmwareEndpointTest.kt
+++ src/test/kotlin/info/szadkowski/sensor/firmware/updater/api/FirmwareEndpointTest.kt
@@ -4,6 +4,7 @@ import io.micronaut.context.annotation.Property
 import io.micronaut.context.annotation.PropertySource
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Header
 import io.micronaut.http.client.annotation.Client
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import org.junit.jupiter.api.Test
@@ -14,7 +15,7 @@ import javax.inject.Inject
 @MicronautTest
 @PropertySource(
     Property(name = "firmware.devices[0].id", value = "a-1"),
-    Property(name = "firmware.devices[0].versions[0]", value = "0.1"),
+    Property(name = "firmware.devices[0].versions[0]", value = "0.2"),
 )
 class FirmwareEndpointTest(
     @Inject private val client: FirmwareClient,
@@ -22,14 +23,21 @@ class FirmwareEndpointTest(
 
     @Test
     fun `not found version is marked as not modified`() {
-        val firmware = client.firmware("a-0")
+        val firmware = client.firmware(device = "a-0", "0.1")
+
+        expectThat(firmware).isEqualTo(HttpStatus.NOT_MODIFIED)
+    }
+
+    @Test
+    fun `same version is marked as not modified`() {
+        val firmware = client.firmware(device = "a-1", version = "0.2")
 
         expectThat(firmware).isEqualTo(HttpStatus.NOT_MODIFIED)
     }
 
     @Test
     fun `return newer firmware`() {
-        val firmware = client.firmware("a-1")
+        val firmware = client.firmware(device = "a-1", version = "0.1")
 
         expectThat(firmware).isEqualTo(HttpStatus.OK)
     }
@@ -38,6 +46,9 @@ class FirmwareEndpointTest(
     interface FirmwareClient {
 
         @Get(uri = "/firmware/{device}", consumes = ["text/plain"])
-        fun firmware(device: String): HttpStatus
+        fun firmware(
+            device: String,
+            @Header(name = "x-ESP8266-version") version: String,
+        ): HttpStatus
     }
 }
