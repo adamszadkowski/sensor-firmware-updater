9e73f9ed65b487b5e43427c88de920f4d80ca8a6
Adam Szadkowski <adam@szadkowski.info>
2021-05-07 18:13:41 +0200

    Cleanup MeterFilterFactory

--- src/main/kotlin/info/szadkowski/sensor/firmware/updater/configuration/MeterFilterFactory.kt
+++ src/main/kotlin/info/szadkowski/sensor/firmware/updater/configuration/MeterFilterFactory.kt
@@ -2,16 +2,13 @@ package info.szadkowski.sensor.firmware.updater.configuration
 
 import io.micrometer.core.instrument.Tag
 import io.micrometer.core.instrument.config.MeterFilter
-import io.micronaut.context.annotation.Bean
 import io.micronaut.context.annotation.Factory
 import io.micronaut.context.annotation.Value
 import javax.inject.Singleton
 
-
 @Factory
 class MeterFilterFactory {
 
-    @Bean
     @Singleton
     fun addCommonTagFilter(@Value("\${micronaut.application.name}") name: String): MeterFilter =
         MeterFilter.commonTags(listOf(Tag.of("application", name)))
