508fdc07cb2f678624200f093ea1b59f811ae702
Adam Szadkowski <adam@szadkowski.info>
2021-05-07 18:13:56 +0200

    Create FirmwareProperties on startup

--- src/main/kotlin/info/szadkowski/sensor/firmware/updater/configuration/FirmwareRepositoryFactory.kt
+++ src/main/kotlin/info/szadkowski/sensor/firmware/updater/configuration/FirmwareRepositoryFactory.kt
@@ -3,6 +3,7 @@ package info.szadkowski.sensor.firmware.updater.configuration
 import info.szadkowski.sensor.firmware.updater.domain.FirmwareRepository
 import info.szadkowski.sensor.firmware.updater.infrastructure.ResourceFirmwareRepository
 import io.micronaut.context.annotation.ConfigurationProperties
+import io.micronaut.context.annotation.Context
 import io.micronaut.context.annotation.Factory
 import io.micronaut.core.io.scan.ClassPathResourceLoader
 import javax.inject.Inject
@@ -18,17 +19,18 @@ class FirmwareRepositoryFactory(
         ResourceFirmwareRepository(resourceLoader, firmwareProperties)
 }
 
+@Context
 @ConfigurationProperties("firmware")
 class FirmwareProperties {
     var devices: List<Device> = emptyList()
 
     class Device {
-        val id: String = ""
-        val versions: List<Version> = emptyList()
+        var id: String = ""
+        var versions: List<Version> = emptyList()
 
         class Version {
-            val version: String = ""
-            val path: String = ""
+            var version: String = ""
+            var path: String = ""
         }
     }
 }
